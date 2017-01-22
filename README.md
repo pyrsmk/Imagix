5e69e32302b292c42656a05f92790f9433767391
pyrsmk <dev@dreamysource.fr>
2017-01-22 10:30:33 +0100

    Move mimetype detection in Imagix\Factory to Funktions project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Imagix 0.6.4
+Imagix 0.6.5
 ============
 
 Imagix is a library, based on GD, that aims to simplify the use of effects on images. This project is born with two ideas in mind :
--- src/Factory.php
+++ src/Factory.php
@@ -14,21 +14,7 @@ class Factory{
 			Imagix\Image
 	*/
     static public function forge($path){
-        // Get image mimetype
-        if(filter_var($path, FILTER_VALIDATE_URL)) {
-            $request = curl_init($path);
-            curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($request, CURLOPT_NOBODY, true);
-            curl_setopt($request, CURLOPT_SSL_VERIFYHOST, false);
-            curl_setopt($request, CURLOPT_SSL_VERIFYPEER, false);
-            curl_exec($request);
-            $mimetype = curl_getinfo($request, CURLINFO_CONTENT_TYPE);
-        }
-        else {
-            $mimetype = getimagesize($path)['mime'];
-        }
-        // Load image
-        switch($mimetype) {
+        switch(mimetype($path)) {
             case 'image/jpeg':
                 return new Image(new Adapter\JPEG($path));
                 break;
